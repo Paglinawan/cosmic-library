87a4c3eb656fe96d4c07eb6ca722aa0faabb2c8a
miho <paglinawan0520@gmail.com>
2025-01-25 09:41:53 +0900

    你好, @svgr/webpack!

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   assetPrefix: "/cosmic-library",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@
 
 import { useEffect } from "react";
 import { createBg } from "./three";
+import Logo from "@/app/assets/images/logo.svg";
 
 export default function Home() {
   useEffect(() => {
     createBg();
   }, []);
-  return <div></div>;
+  return (
+    <div className="fixed left-4 top-4 z-10">
+      <Logo className="w-16 h-16" />
+    </div>
+  );
 }
