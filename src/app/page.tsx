b8c8b5326f398e636710b0f938b1f7bdef01cec5
miho <paglinawan0520@gmail.com>
2025-01-26 15:14:08 +0900

    Test: remove Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,10 @@
 
 import { useEffect } from "react";
 import { createBg } from "./three";
-import Logo from "@/app/assets/images/logo.svg";
 
 export default function Home() {
   useEffect(() => {
     createBg();
   }, []);
-  return (
-    <div className="fixed left-4 top-4 z-10">
-      <Logo className="w-16 h-16" />
-    </div>
-  );
+  return <div className="fixed left-4 top-4 z-10"></div>;
 }
